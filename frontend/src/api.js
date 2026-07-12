af7c66baeed44339a3d01c3a5ee3c227a6d98ee5
SwayamY <148499030+SwayamY@users.noreply.github.com>
2026-07-12 16:17:42 +0530

    edited setToken function to manage token storage

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -4,7 +4,9 @@ export function getToken() {
   return localStorage.getItem('token');
 }
 
-
+export function setToken(token) {
+  localStorage.setItem('token', token);
+}
 
 export function clearToken() {
   localStorage.removeItem('token');
